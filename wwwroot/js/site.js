4c219853432a18a2616cec3dec83598121512b67
Oto Okino <108208574+okino-oto@users.noreply.github.com>
2023-08-31 16:42:56 +0900

    Update site.js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function refreshTokens() {
+  let refreshUrl = "/.auth/refresh";
+  $.ajax(refreshUrl) .done(function() {
+    console.log("Token refresh completed successfully.");
+  }) .fail(function() {
+    console.log("Token refresh failed. See application logs for details.");
+  });
+}
